3fb33183238927010d1c98aafc914e85a88d2f94
Shaun Porwal <shaun.porwal@gmail.com>
2025-02-15 23:22:13 -0500

    Finalize monkey script, update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # blender-llm-testing
 Repo to test using cursor llms with blender
+
+
+The hello.py file is a script that creates a 3D model of a monkey in Blender. It uses the bpy library to interact with Blender's Python API.
+
+To run the script, you need to have Blender installed. You can then run the script using the following command:
+
+```
+blender --python hello.py
+```
--- hello.py
+++ hello.py
@@ -53,52 +53,10 @@ subsurf.levels = 2  # Viewport subdivision levels
 subsurf.render_levels = 2  # Render subdivision levels
 subsurf.quality = 3  # Catmull-Clark subdivision
 
-# Add empty object as center point
-bpy.ops.object.empty_add(type='PLAIN_AXES', location=(0, 0, 0))
-center = bpy.context.active_object
+# Unparent the monkey so that it can rotate in place (remove empty influence)
+monkey.parent = None
 
-# Parent monkey to the empty
-monkey.parent = center
-# Make the monkey face outward and stay upright
-monkey.rotation_euler = (0, 0, radians(90))
-
-# Animation settings
-frame_start = 1
-frame_end = 120  # 5 seconds at 24fps
-bpy.context.scene.frame_start = frame_start
-bpy.context.scene.frame_end = frame_end
-# Set animation to loop
-bpy.context.scene.render.fps = 25
-bpy.context.scene.frame_current = frame_start
-bpy.context.scene.use_preview_range = False
-bpy.context.scene.frame_set(frame_start)
-
-# Enable cyclic animation
-action = bpy.data.actions.new(name="MonkeyRotation")
-center.animation_data_create()
-center.animation_data.action = action
-
-# Create the rotation animation
-for frame in range(frame_start, frame_end + 1):
-    bpy.context.scene.frame_set(frame)
-    
-    # Calculate rotation angle
-    angle = radians((frame - 1) * (360.0 / frame_end))
-    
-    # Set empty's rotation
-    center.rotation_euler.z = angle
-    
-    # Insert keyframe for rotation
-    center.keyframe_insert(data_path="rotation_euler", frame=frame)
-
-# Set up cyclic animation
-for fc in center.animation_data.action.fcurves:
-    if fc.data_path == "rotation_euler":
-        # Set the curve to cycle
-        fc.modifiers.new('CYCLES')
-        # Make the animation linear
-        for kf in fc.keyframe_points:
-            kf.interpolation = 'LINEAR'
+# Material creation and coloration code removed per request
 
 # Add some basic lighting
 bpy.ops.object.light_add(type='SUN', location=(5, 5, 10))
@@ -110,8 +68,3 @@ camera.rotation_euler = (radians(75), 0, radians(90))
 
 # Set the new camera as the active camera
 bpy.context.scene.camera = camera
-
-# Optional: Add a simple material to the monkey
-material = bpy.data.materials.new(name="MonkeyMaterial")
-material.use_nodes = True
-monkey.data.materials.append(material)
